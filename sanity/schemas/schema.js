9761b51acedca9b6ea584d6ca5de68d01c2ac1be
Luiz Carneiro <luuizpaulo@gmail.com>
2020-10-13 19:25:02 +0200

    small change in sanity schema

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -6,6 +6,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type';
 export default createSchema({
   // We name our schema
   name: 'default',
+  age: '12y',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([]),
