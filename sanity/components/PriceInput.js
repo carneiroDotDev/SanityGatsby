f7febf928b78aa4d6eaf74a01fd5df6ddeb0bfef
Luiz Carneiro <luuizpaulo@gmail.com>
2020-10-19 21:57:39 +0200

    Started building sanity as cms locally

---
+++ sanity/components/PriceInput.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PatchEvent, { set, unset } from "part:@sanity/form-builder/patch-event"
+
+function craetePatchFrom(value) {
+  return PatchEvent.from(value === '' ? unset() : set(Number(value)))
+}
+
+const formatMoney = Intl.NumberFormat('en-CA', {
+  style: 'currency',
+  currency: 'CAD',
+}).format;
+
+export default function PriceInput({ type, value, onChange, inputComponent }) {
+  return (
+    <div>
+      <h2>{type.title} - {value && formatMoney(value / 100)} </h2>
+      <p>{type.description}</p>
+      <input
+        type={type.name}
+        value={value}
+        onChange={event => onChange(craetePatchFrom(event.target.value))}
+        ref={ inputComponent }/>
+    </div>
+  )
+}
+
+PriceInput.focus = function () {
+  this._inputElement.focus()
+}
